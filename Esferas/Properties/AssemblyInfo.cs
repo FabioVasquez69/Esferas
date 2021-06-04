00e16178dfb5d28cd5fece7c390ef3f3dc352df0
FabioVasquez69 <fabio.vaz93@hotmail.com>
2021-06-04 09:18:03 -0500

    Esferas

---
+++ Esferas/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Esferas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Esferas")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba6d4ec7-abf8-4a6e-b563-0937edea6fe6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Esferas/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using FluentAssertions;
+using System.Threading;
+using SeleniumExtras.WaitHelpers;
+
+
+namespace Esferas
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ChromeDriver driver;
+        [TestMethod]
+        public void BotonInhabilitado()
+        {
+            ChromeOptions options = new ChromeOptions;
+            options.AddArgument("start-maximazed");
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://demoqa.com/dynamic-properties");
+
+            //driver.FindElement(By.XPath("//input[@id='enableAfter']")).Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(4));
+            //wait.Until(ExpectedConditions.ElementIsVisible);
+
+            IWebElement botonInvisible = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//button[@id='visibleAfter']")));
+            Console.WriteLine(botonInvisible.Text);
+
+            IWebElement miOtroElemento -wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("")));
+            miOtroElemento.Click();
+
+        }
+
+        public void Cerrar()
+        {
+
+            Thread.Sleep(6000);
+            driver.Quit();
+        }
+    }
+
+}
